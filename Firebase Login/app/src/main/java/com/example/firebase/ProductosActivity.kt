8c89344f80d1dec42d47dacc5954d9f0b04c69ff
Vladimir7w7 <107089945+Vladimir7w7@users.noreply.github.com>
2023-04-03 06:26:03 -0600

    Update

---
+++ Firebase Login/app/src/main/java/com/example/firebase/ProductosActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class ProductosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+    }
+
+}
